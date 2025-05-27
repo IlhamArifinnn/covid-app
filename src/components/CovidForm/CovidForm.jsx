fbb1cfa9f7c6e1ab1919a3bf5206e82065d93314
IlhamArifinnn <ilha23244ti@student.nurulfikri.ac.id>
2025-05-27 20:39:43 +0700

    build: covid app

---
+++ src/components/CovidForm/CovidForm.jsx
@@ -0,0 +1,119 @@
+import { useState } from "react";
+import Button from "../Button/Button";
+import data from "../../utils/constants/provinces";
+
+function CovidForm({ covid, setCovid }) {
+  const [provinsi, setProvinsi] = useState("");
+  const [status, setStatus] = useState("");
+  const [jumlah, setJumlah] = useState();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const updatedProvinces = covid.provinces.map((item) => {
+      if (item.provinsi === provinsi) {
+        // Salin data provinsi lama dan update status sesuai input
+        return {
+          ...item,
+          [status]: item[status] + parseInt(jumlah, 10),
+        };
+      }
+      return item;
+    });
+
+    setCovid({
+      ...covid,
+      provinces: updatedProvinces,
+    });
+
+    // Reset form
+    setProvinsi("");
+    setStatus("");
+    setJumlah("");
+  }
+
+  return (
+    <section
+      id="covid-form"
+      className="bg-blue-50 p-5 shadow-md flex flex-col gap-6 md:flex-row"
+    >
+      <div className="flex items-center justify-center md:w-1/2">
+        <img
+          src="/consultation.png"
+          alt="Form ilustration"
+          className="w-[70%] max-w-md h-auto object-contain md:w-[80%]"
+        />
+      </div>
+
+      <div className="md:w-1/2 flex flex-col justify-center">
+        <h1 className="text-3xl font-bold text-center text-blue-700">
+          Form Covid
+        </h1>
+
+        <form className="p-5 flex flex-col gap-4" onSubmit={handleSubmit}>
+          <div className="flex flex-col">
+            <label htmlFor="provinsi" className="text-blue-600 mb-1">
+              Provinsi
+            </label>
+            <select
+              name="provinsi"
+              id="provinsi"
+              value={provinsi}
+              onChange={(e) => setProvinsi(e.target.value)}
+              className="p-2 border rounded-lg"
+              required
+            >
+              <option value="">Pilih Provinsi</option>
+              {data.provinces.map((item) => (
+                <option key={item.provinsi} value={item.provinsi}>
+                  {item.provinsi}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="flex flex-col">
+            <label htmlFor="status" className="text-blue-600 mb-1">
+              Status
+            </label>
+            <select
+              name="status"
+              id="status"
+              value={status}
+              onChange={(e) => setStatus(e.target.value)}
+              className="p-2 border rounded-lg"
+              required
+            >
+              <option value="">Pilih Status</option>
+              <option value="positif">Positif</option>
+              <option value="sembuh">Sembuh</option>
+              <option value="dirawat">Dirawat</option>
+              <option value="meninggal">Meninggal</option>
+            </select>
+          </div>
+
+          <div className="flex flex-col">
+            <label htmlFor="jumlah" className="text-blue-600 mb-1">
+              Jumlah
+            </label>
+            <input
+              type="number"
+              name="jumlah"
+              id="jumlah"
+              min="0"
+              value={jumlah}
+              onChange={(e) => setJumlah(e.target.value)}
+              placeholder="Masukan Jumlah"
+              className="p-2 border rounded-lg"
+              required
+            />
+          </div>
+
+          <Button type="submit">Submit</Button>
+        </form>
+      </div>
+    </section>
+  );
+}
+
+export default CovidForm;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toogleMenu = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  return (
+    <nav
+      className="bg-blue-400 text-white p-4 flex flex-col sm:flex-row items-center justify-between md:justify-around sticky top-0 right-0 left-0 z-100"
+      aria-label="Main Navigation"
+    >
+      <div className="flex items-center justify-between w-full sm:w-auto mb-2 sm:mb-0">
+        <a href="#hero" className="flex items-center gap-2">
+          <img src="/coronavirus.png" alt="Corona Virus" className="w-10" />
+          <h1 className="text-2xl font-bold">Covid ID</h1>
+        </a>
+        {/* Toggle button for small screens */}
+        <button
+          className="sm:hidden text-white focus:outline-none mr-4"
+          onClick={toogleMenu}
+          aria-label="Toggle Navigation Menu"
+        >
+          {isOpen ? <X size={28} /> : <Menu size={28} />}
+        </button>
+      </div>
+
+      {/* Navigation links */}
+      <ul
+        className={`${
+          isOpen ? "flex" : "hidden"
+        } sm:flex flex-col sm:flex-row gap-4 sm:gap-6 items-center`}
+      >
+        <li>
+          <a href="#hero" className="hover:underline">
+            Global
+          </a>
+        </li>
+        <li>
+          <a href="#indonesia" className="hover:underline">
+            Indonesia
+          </a>
+        </li>
+        <li>
+          <a href="#provinsi" className="hover:underline">
+            Provinsi
+          </a>
+        </li>
+        <li>
+          <a href="#footer" className="hover:underline">
+            Tentang Kami
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
